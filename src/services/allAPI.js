fc81f689b90298afe4b232039420f0f2b264e7d8
VIVEK E N <viveken4902@gmail.com>
2024-04-18 23:47:47 +0530

    new

--- src/services/allAPI.js
+++ src/services/allAPI.js
@@ -1,5 +1,6 @@
-import { SERVER_URL } from "./SERVER_URL"
+
 import { commonAPI } from "./commonAPI"
+import { SERVER_URL } from "./serverURL"
 
 export const registerAPI = async (reqBody) =>{
     return await commonAPI("POST",`${SERVER_URL}/register`,reqBody)
